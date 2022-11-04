2b6b7a688af247860fa2c85c1277b083cd17a849
vulnerable <82110489+lagcompensation@users.noreply.github.com>
2022-11-04 19:07:45 +0300

    removed functions related to prediction

--- csgo_sdk/local/prediction.h
+++ csgo_sdk/local/prediction.h
@@ -5,8 +5,6 @@ namespace hacks {
 	class c_prediction {
 	public:
 		void update();
-		void start(c_user_cmd* cmd);
-		void end(c_user_cmd* cmd);
 	};
 
 	inline const auto g_prediction = std::make_unique<c_prediction>();
